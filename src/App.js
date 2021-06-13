705f23ef607666da8094918209767082653d36f6
dimagmercan <dimagm@gmail.com>
2021-06-13 11:37:15 +0200

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <h1>Weather Search Engine</h1>
-
-        <Search />
       </header>
+
+      <Search />
+
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/dimagmercan/" target="_blank">
+          Dimag Mercan
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/dimagmercan/weather-react" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
