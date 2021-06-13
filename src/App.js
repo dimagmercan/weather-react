7a86feb2119d7fb738dd05166aa4d53830ddd938
dimagmercan <dimagm@gmail.com>
2021-06-13 11:28:54 +0200

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Search from "./Search";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -12,5 +12,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
